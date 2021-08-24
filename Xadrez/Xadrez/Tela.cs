9dfda08ca9a9bad95f4e4258474d68fb6a1b6d60
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2021-08-24 20:09:45 -0300

    Primeiro commit

---
+++ Xadrez/Xadrez/Tela.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using tabuleiro;
+
+namespace Xadrez
+{
+    class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                Console.Write(8 - i + " ");
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (tab.peca(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+        }
+    }
+}
