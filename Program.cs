647c0bc00f175a085894c5d69ec081edd5ac88b7
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2021-09-23 14:19:17 -0300

    Correção na organização do codigo

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executarMoimento(origem, destino);
+                }
+
+                Tela.imprimirTabuleiro(partida.tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,26 @@
+﻿namespace tabuleiro
+{
+    public abstract class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Tabuleiro tab, Cor cor)
+        {
+            this.tab = tab;
+            this.cor = cor;
+            this.posicao = null;
+            this.qteMovimentos = 0;
+        }
+
+        public void incrementarQtdMovimentos()
+        {
+            qteMovimentos++;
+        }
+
+        public abstract bool[,] movimentosPossiveis();
+
+    }
+}
